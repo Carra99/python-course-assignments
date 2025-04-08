b4cbb62d3f04623bb5d3cfc6f5583480fc87bf62
Carra99 <145321372+Carra99@users.noreply.github.com>
2025-04-08 13:39:32 +0300

    Update rectangle.py

--- day02/rectangle.py
+++ day02/rectangle.py
@@ -1,1 +1,8 @@
+#calculate area and perimeter of a rectangle 
 
+height = float(input("Enter the height of the rectangle: "))
+width = float(input("Enter the width of the rectangle: ")) 
+area = height * width
+perimeter = 2 * (height + width)
+
+print(f"the area of the rectangle is {area:.2f} cm² and the perimeter is {perimeter:.2f} cm.")
