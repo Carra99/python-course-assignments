1ce4fa38e746c1ada6f0dd0caa1e02e9dc4d4fd5
Carra99 <145321372+Carra99@users.noreply.github.com>
2025-04-08 13:37:57 +0300

    Update circle.py

--- day02/circle.py
+++ day02/circle.py
@@ -6,4 +6,4 @@ radius = float(input("Enter the radius of the circle in cm: "))
 area = np.pi*radius**2
 circumference = 2*np.pi*radius
 
-print(f"the circle of radius {radius} cm has an area of {area:.2f} cm², and a circumference of {circumference:.2f} cm")
+print(f"the circle has an area of {area:.2f} cm², and a circumference of {circumference:.2f} cm.")
