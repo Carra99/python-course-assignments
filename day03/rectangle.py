34a80507dcb1adfd5e7f383997794e911b1bfd40
Carra99 <145321372+Carra99@users.noreply.github.com>
2025-04-23 14:05:27 +0300

    Update rectangle.py

--- day03/rectangle.py
+++ day03/rectangle.py
@@ -2,8 +2,17 @@
 
 import sys
 
-height = float(input("Enter the height of the rectangle in cm: "))
-width = float(input("Enter the width of the rectangle in cm: ")) 
+if len(sys.argv)<3:
+  print("Usage: python rectangle.py <height_in_cm> <width_in_cm>")
+  sys.exit(1)
+
+try:
+  height = float(sys.argv[1])
+  width = float(sys.argv[2])
+except ValueError:
+  print("Please provide valid numbers for height and width")
+  sys.exit(1)
+
 area = height*width
 perimeter = 2*(height + width)
 
