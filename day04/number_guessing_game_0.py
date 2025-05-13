ad08573be39f62b2ab847c4b21ec11a6122cbf40
Carra99 <145321372+Carra99@users.noreply.github.com>
2025-05-13 12:01:47 +0300

    Update number_guessing_game_0.py

--- day04/number_guessing_game_0.py
+++ day04/number_guessing_game_0.py
@@ -1,1 +1,15 @@
+import random as rand
 
+numb = rand.randint(1, 20)
+print("------------------------------------")
+print("Welcome to the number guessing game!")
+print("------------------------------------")
+guess = int(input("Guess a number between 1 and 20: "))
+
+if numb > guess:
+    print("Your guess is too low.")
+elif numb < guess:
+    print("Your guess is too high.")
+else:
+    print("Congratulations! You guessed the number!")
+print("Thanks for playing!")
