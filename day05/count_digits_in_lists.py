77bb914a32b7b96b259cd9783341359d87ba2bc8
Carra99 <145321372+Carra99@users.noreply.github.com>
2025-05-22 14:55:57 +0300

    Update count_digits_in_lists.py

--- day05/count_digits_in_lists.py
+++ day05/count_digits_in_lists.py
@@ -1,1 +1,12 @@
+numbers = [1203, 1256, 312456, 98]
 
+counts = [0] * 10
+
+for number in numbers:
+    for digit in str(number):
+        counts[int(digit)] += 1
+
+print("Digit Count")
+
+for i, count in enumerate(counts):
+    print(f"{i}  {count}")
