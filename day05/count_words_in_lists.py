e17046bdc7483acbb12a861c8f56a824afde9d93
Carra99 <145321372+Carra99@users.noreply.github.com>
2025-05-22 15:07:18 +0300

    Update count_words_in_lists.py

--- day05/count_words_in_lists.py
+++ day05/count_words_in_lists.py
@@ -1,1 +1,9 @@
+celestial_objects = ['Moon', 'Gas', 'Asteroid', 'Dwarf', 'Asteroid', 'Moon', 'Asteroid']
 
+word_count = {}
+for word in celestial_objects:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+print(word_count)
