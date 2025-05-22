89bc6a72c51246fdb7b4719f8ebb775998be01bb
Carra99 <145321372+Carra99@users.noreply.github.com>
2025-05-22 14:22:23 +0300

    Update extended_dna_sequencing.py

--- day05/extended_dna_sequencing.py
+++ day05/extended_dna_sequencing.py
@@ -1,1 +1,25 @@
+sequence = input("Please enter a DNA sequence: ").upper()
+valid_chars = {"A", "C", "G", "T"}
 
+if all(c not in valid_chars for c in sequence):
+    print("No valid DNA characters found. Accepted characters: A, C, G, T")
+    exit()
+
+fragments = []
+current = [] 
+
+for c in sequence: 
+    if c in valid_chars:
+        current.append(c)
+    else:
+        if current:
+            fragments.append("".join(current))
+            current = []
+
+if current:
+    fragments.append("".join(current))
+
+sorted_fragments = sorted(fragments, key=len, reverse=True) 
+
+print(sorted_fragments)    
+ 
