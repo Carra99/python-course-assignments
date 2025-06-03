0c8fee0166e89c6ef9c61d547e4b1c2e770e46b7
Carra99 <145321372+Carra99@users.noreply.github.com>
2025-06-03 22:42:04 +0300

    Update count_words_in_lists_with_functions.py

--- day07/count_words_in_lists_with_functions.py
+++ day07/count_words_in_lists_with_functions.py
@@ -1,1 +1,22 @@
+def get_celestial_objects():
+    return ['Moon', 'Gas', 'Asteroid', 'Dwarf', 'Asteroid', 'Moon', 'Asteroid']
 
+def count_words(words):
+    count = {}
+    for word in words:
+        if word in count:
+            count[word] += 1
+        else:
+            count[word] = 1
+    return count
+
+def print_word_count(counts):
+    print(counts)
+
+def main():
+    objects = get_celestial_objects()
+    word_counts = count_words(objects)
+    print_word_count(word_counts)
+
+if __name__ == "__main__":
+    main()
